a1cbbafd09b12a6ab75c552bcf808be860ffcc6b
getahsaan <getahsaan@gamil.com>
2023-04-12 03:47:58 +0600

    added job details

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,13 +1,13 @@
 export const jobCategoryData = async () => {
-  const res1 = await fetch("jobCategoryData.json");
+  const res1 = await fetch("/jobCategoryData.json");
   const jobCategoryData = await res1.json();
-  const res2 = await fetch("featuredJobsData.json");
+  const res2 = await fetch("/featuredJobsData.json");
   const featuredJobsData = await res2.json();
   return { jobCategoryData, featuredJobsData };
 };
 
 export const jobDetailsLoader = async ({ params }) => {
-  const res2 = await fetch("featuredJobsData.json");
+  const res2 = await fetch("/featuredJobsData.json");
   const featuredJobsData = await res2.json();
   const targetJob = featuredJobsData.find((jd) => jd.id == params.id);
   return targetJob;
@@ -18,7 +18,7 @@ export const appliedJobsLoader = async () => {
   if (!storedJob) {
     storedJob = [];
   }
-  const res2 = await fetch("featuredJobsData.json");
+  const res2 = await fetch("/featuredJobsData.json");
   const featuredJobsData = await res2.json();
   const appliedJobData = [];
   featuredJobsData.forEach((fdata) => {
--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -8,14 +8,45 @@ const JobDetails = () => {
     addToLocalStorageDb(id);
   };
   return (
-    <div className="container mx-auto text-center">
-      Job Details
-      <button
-        onClick={() => handleApplyJob(targetJobs.id)}
-        className="btn-custom"
-      >
-        Apply JOb
-      </button>
+    <div className="container mx-auto w-[90%] md:w-[80%]">
+      <h1 className="text-center text-4xl md:text-6xl my-10">Job Details</h1>
+      <div className="md:flex items-center justify-between gap-8">
+        <div className="w-3/4">
+          <p className="my-4">
+            <span className="font-bold">Job Description : </span>{" "}
+            {targetJobs.job_description}
+          </p>
+          <p className="my-4">
+            <span className="font-bold">Job Responsibility : </span>{" "}
+            {targetJobs.job_responsibilities}
+          </p>
+          <p className="my-4">
+            <span className="font-bold">Educational Requirements : </span>{" "}
+            {targetJobs.educational_requirements}
+          </p>
+          <p className="my-4">
+            <span className="font-bold">Job Experience : </span>{" "}
+            {targetJobs.experience}
+          </p>
+        </div>
+        <div className="bg-indigo-100 md:w-1/4 p-6 rounded-lg">
+          <h2 className="font-bold">Job Details</h2>
+          <hr className="bg-indigo-300 h-[2px] my-4" />
+          <p><span className="font-semibold">Salary : </span>{targetJobs.salary}</p>
+          <p><span className="font-semibold">Job Title : </span>{targetJobs.title}</p>
+          <h2 className="font-bold mt-4">Contact Information</h2>
+          <hr className="bg-indigo-300 h-[2px] my-4" />
+          <p><span className="font-semibold">Phone : </span>{targetJobs.phone}</p>
+          <p><span className="font-semibold">Email : </span>{targetJobs.email}</p>
+          <p><span className="font-semibold">Address : </span>{targetJobs.address}</p>
+          <button
+            onClick={() => handleApplyJob(targetJobs.id)}
+            className="btn-custom md:w-full w-[50%] mt-4"
+          >
+            Apply Now
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
